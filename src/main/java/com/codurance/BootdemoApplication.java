315cb7226b78b65657f79e13c31a2edff8ca140b
Tom <thomas.spencer@codurance.com>
2021-06-14 20:03:32 +0100

    Add code for all routes

--- src/main/java/com/codurance/BootdemoApplication.java
+++ src/main/java/com/codurance/BootdemoApplication.java
@@ -2,9 +2,18 @@ package com.codurance;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,9 +25,58 @@ public class BootdemoApplication {
 }
 
 @RestController
-@RequestMapping("/coffees")
+@RequestMapping("/")
 class RestApiDemoController {
   private List<Coffee> coffees = new ArrayList<>();
+
+  public RestApiDemoController(){
+    coffees.addAll(List.of(
+        new Coffee("Café Cereza"),
+        new Coffee("Café Ganador"),
+        new Coffee("Café Lareño"),
+        new Coffee("Café Três Pontas")
+    ));
+  }
+
+  @GetMapping("/coffees")
+  Iterable<Coffee> getCoffees(){
+    return coffees;
+  }
+
+  @GetMapping("/coffees/{id}")
+  Optional<Coffee> getCoffeeById(@PathVariable String id) {
+    for (Coffee c : coffees){
+      if (c.getId().equals(id)){
+        return Optional.of(c);
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  @PostMapping("/coffees")
+  Coffee postCoffee(@RequestBody Coffee coffee) {
+    coffees.add(coffee);
+    return coffee;
+  }
+
+  @PutMapping("/coffees/{id}")
+  Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+    int coffeeIndex = -1;
+
+    for (Coffee c : coffees){
+      if (c.getId().equals(id)){
+        coffeeIndex = coffees.indexOf(c);
+        coffees.set(coffeeIndex, coffee);
+      }
+    }
+    return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+  }
+
+  @DeleteMapping("/coffees/{id}")
+  void deleteCoffee(@PathVariable String id) {
+    coffees.removeIf(c -> c.getId().equals(id));
+  }
 }
 
 class Coffee {
@@ -27,6 +85,22 @@ class Coffee {
 
   public Coffee(String id, String name){
     this.id = id;
+    this.name = name;
+  }
 
+  public Coffee(String name){
+    this(UUID.randomUUID().toString(), name);
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String name){
+    this.name = name;
   }
 }
